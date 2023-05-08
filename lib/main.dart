b4fc26693c7b8675dd2922e5ff62125c5f80641b
Lukaslakas31 <lukejuniel@gmail.com>
2023-05-08 15:09:12 +0800

    demo

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Image Uploader',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
